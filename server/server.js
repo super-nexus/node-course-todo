d7b1470108e75ab7a5059ca361c796441ad4ecf1
Andrija <andrija.kuzmanov@gmail.com>
2017-10-21 22:09:57 +0200

    Prepared app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -54,8 +55,8 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 })
 
 
