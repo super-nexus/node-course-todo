aebfa7744d1cbadc06174a620c01ac529683f7d6
Andrija <andrija.kuzmanov@gmail.com>
2017-10-22 12:46:09 +0200

    Prepared app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://nexus2:proshifters@ds227035.mlab.com:27035/node-todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
