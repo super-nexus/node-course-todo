8ca7ad69da903794ac0e3a3cf83f1461a690bab4
Andrija <andrija.kuzmanov@gmail.com>
2017-12-22 22:48:54 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,9 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var data = {
   id: 10
 };
+
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (error, hash) => {
+//       console.log(hash);
+//   })
+// })
+
+
+var hashedPassoword = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTUxMzk3ODQwMH0.H9aXRbIL6HIV4K37kz-P3kBIx8jPYJVBjpa4iBggcVA';
+
+bcrypt.compare(password, hashedPassoword, (err, res) => {
+  console.log(res);
+})
 
 var token = jwt.sign(data, '123abc')
 console.log(token);
