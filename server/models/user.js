131a3210b82b243fd138da369ff1d33c0e2cbda9
Andrija <andrija.kuzmanov@gmail.com>
2017-12-23 19:14:00 +0100

    Added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -62,6 +62,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+      $pull: {
+        tokens: {
+          token
+        }
+      }
+    });
+}
+
 UserSchema.statics.findByToken = function (token){
 
   var User = this;
